649d1002106ffdc1b306b9a5fa9cbcea6d94393b
Arman Mamian <armanmamian@gmail.com>
2020-04-28 19:00:59 +0300

    Update asm.h

--- include/asm.h
+++ include/asm.h
@@ -1,6 +1,15 @@
 #ifndef ASM_H
 # define ASM_H
 
-#include <unistd.h>
+# define BUFF_SIZE 32767
+
+typedef struct		s_asm {
+	int				i;
+	int				j;
+	int				nl;
+	char			buff[BUFF_SIZE];
+	int				r;
+	int				fd;
+}					t_asm;
 
 #endif
